3d743dcd0002e615b45a49b743f92e9cd41a819c
jfr10 <jfr10@le.ac.uk>
2020-01-24 16:19:43 +0000

    renaming gedi_explore

--- gedi_to_shp.py
+++ gedi_to_shp.py
@@ -13,13 +13,14 @@ args = parser.parse_args()
 
 gedi_swath = h5py.File(args.filepath, 'r')
 
-for beam in gedi_swath.values():
+for beam in gedi_swath.values()[0:0]:
     try:
         lats = beam["geolocation"]["latitude_bin0"]
         lons = beam["geolocation"]["longitude_bin0"]
         pai = beam["pai"]
 
         df = pd.DataFrame({'pai':pai,
+                           'height':beam['rh100'],
                            'beam':beam['beam'],
                            'Latitude':lats,
                            'Longitude':lons})
